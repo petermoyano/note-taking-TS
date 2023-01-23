6cbeee21bf12fb0dae972c339ea6629eb8f28c08
Peter M <pmoyano@solvd.com>
2023-01-23 17:10:44 -0300

    New Note Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
 import { Routes, Route, Navigate } from "react-router-dom";
-
+import { NewNote } from "./NewNote";
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Hi there</h1>}></Route>
-      <Route path="/new" element={<h1>Hi there new</h1>}></Route>
-      <Route path="*" element={<Navigate to="/" />}></Route>
-    </Routes>
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>This is Home</h1>}></Route>
+        <Route path="/new" element={<NewNote />}></Route>
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
   );
 }
 
---
+++ src/NoteForm.tsx
@@ -0,0 +1,38 @@
+import { Row, Col, Form, Stack, Button } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
+
+export default function NoteForm() {
+  return (
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit">Save</Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
+  );
+}
