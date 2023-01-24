be95d82cd03b2c3e8823abb4cd62b85059cba63b
Peter M <pmoyano@solvd.com>
2023-01-24 10:58:45 -0300

    newNoteForm functionality

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -3,7 +3,7 @@ import NoteForm from "./NoteForm";
 export function NewNote() {
   return (
     <>
-      <h1 className="mb-4">New note</h1>;
+      <h1 className="mb-4">New note</h1>
       <NoteForm />
     </>
   );
