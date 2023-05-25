ea19d23109f8346e39f6e629213d6d99d6c30c75
Peter M <pmoyano@solvd.com>
2023-05-25 20:32:02 -0300

    Add homepage

--- src/ContextContainer.tsx
+++ src/ContextContainer.tsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import ThemeContext from './Context/ThemeContext';
 import styles from './ContextContainer.module.css';
 
-export default function ({ children }: any) {
+export default function ({ children }) {
   const theme = useContext(ThemeContext);
   console.log(theme);
   return <Container className={styles.body}>{children}</Container>;
