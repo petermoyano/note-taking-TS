70dd701ac14135d01784179f63196d2e7ba7da9e
Peter M <pmoyano@solvd.com>
2023-05-26 17:05:45 -0300

    Change build destination

--- src/ContextContainer.tsx
+++ src/ContextContainer.tsx
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap';
 import ThemeContext from './Context/ThemeContext';
 import styles from './ContextContainer.module.css';
 
-export default function ({ children }) {
+export default function ({ children }: any) {
   const theme = useContext(ThemeContext);
   console.log(theme);
   return <Container className={styles.body}>{children}</Container>;
