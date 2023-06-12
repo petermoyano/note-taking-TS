be59398a060ba9f3384d748ce55014ac4429fab6
Peter M <pmoyano@solvd.com>
2023-06-12 10:17:55 -0300

    test

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -45,7 +45,7 @@ export function NoteList({
     <>
       <Row className='align-items-center mb-4'>
         <Col>
-          <h1>Notes</h1>
+          <h1>Notes and test</h1>
         </Col>
         <Col xs='auto'>
           <Stack gap={2} direction='horizontal'>
